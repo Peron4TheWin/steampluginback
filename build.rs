f8bcb9955c03f17e1b217f74216a7a70a0febe6d
Peron4TheWIn <frandonna12@gmail.com>
2026-06-10 13:34:58 -0300

    Move `ws2_32.lib` exclusion from `build.rs` to `.cargo/config.toml` for cleaner build configuration.

--- build.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    // ws2_32.lib define las mismas funciones que exportamos (accept, bind, send, etc.)
-    // causando LNK2005. Lo excluimos porque nosotros somos el wsock32 proxy.
-    println!("cargo:rustc-link-arg=/NODEFAULTLIB:ws2_32.lib");
-}
